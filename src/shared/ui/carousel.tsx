62ca4c8290b316014afb06ca5f7c548354f25772
Bill <dev.anuwong@gmail.com>
2026-08-09 16:28:20 +0700

    refactor: move shared UI primitives and utils to src/shared

--- src/shared/ui/carousel.tsx
+++ src/shared/ui/carousel.tsx
@@ -2,8 +2,8 @@ import useEmblaCarousel, { type UseEmblaCarouselType } from 'embla-carousel-reac
 import { ArrowLeft, ArrowRight } from 'lucide-react'
 import * as React from 'react'
 
-import { Button } from '@/components/ui/button'
-import { cn } from '@/utils/cn'
+import { Button } from '@/shared/ui/button'
+import { cn } from '@/shared/utils/cn'
 
 type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
